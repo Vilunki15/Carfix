bc96ca36fe8ee7103be30236893ae2a41c15b9ba
Vilunki15 <ville.tanska2@gmail.com>
2023-05-10 13:02:57 +0300

    Comments and css

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './App.css';
 
 const Footer = () => {
     return (
--- src/Main.js
+++ src/Main.js
@@ -3,14 +3,17 @@ import Home from './Home';
 import Services from './Services';
 import Gallery from './Gallery';
 import Contact from './Contact';
+import './App.css';
 
 const Main = () => {
+    //Using useState to store current main content
     const [ currentPage, setCurrentPage ] = useState('home');
+    //function for pagechange
     const handlePageChange = (page) => () => {
         setCurrentPage(page);
     }
     let mainContent;
-
+    //Switch case for page selector
     switch (currentPage) {
         case 'home':
             mainContent = <Home />;
@@ -28,14 +31,16 @@ const Main = () => {
             mainContent = <Home />;
             
     }
-
+    //returning navbuttons and selected content. Also social media links
     return (
         <div>
-            <div>
-                <button onClick={handlePageChange('home')}>Etusivu</button>
-                <button onClick={handlePageChange('services')}>Palvelut</button>
-                <button onClick={handlePageChange('gallery')}>Galleria</button>
-                <button onClick={handlePageChange('contact')}>Yhteystiedot</button>
+            <div className='navbar'>
+                <button onClick={handlePageChange('home')}>ETUSIVU</button>
+                <button onClick={handlePageChange('services')}>PALVELUT</button>
+                <button onClick={handlePageChange('gallery')}>GALLERIA</button>
+                <button onClick={handlePageChange('contact')}>YHTEYSTIEDOT</button>
+                <button><a href='https://www.facebook.com/CarFixoulainen/' target='_blanck' rel='noreferrer noopener'>FACEBOOK</a></button>
+                <button><a href='https://www.instagram.com/carfix_oulainen/' target='_blanck' rel='noreferrer noopener'>INSTAGRAM</a></button>
             </div>
             <div>
                 {mainContent}
