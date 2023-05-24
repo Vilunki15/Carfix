fc397f1f538d20349efdfcf43e9da6e91469bef9
Vilunki15 <ville.tanska2@gmail.com>
2023-05-24 09:54:35 +0300

    npm Helmet library and og metadata added

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 import './Assets/Font/fonts.css';
+import { Helmet } from 'react-helmet';
 import logo from './Assets/Img/headerImg.jpg'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <img src={logo} alt='CarFix Oulainen Logo' />
+        <div> 
+            <div className='metadata'>
+                <Helmet>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <meta name="description" content="Autonpesu- ja fixauspalvelut laidasta laitaan teille tarjoaa CarFix Oulainen!"/>
+                    <meta name="keywords" content="Autonpesu, carifix, fix, oulainen, auto, likainen, autot, fixaus"/>
+                    <meta name="author" content="CarFix Oulainen"/>
+                    <meta property="og:title" content="CarFix Oulainen"/>
+                    <meta property="og:description" content="Autonpesu- ja fixauspalvelut laidasta laitaan teille tarjoaa CarFix Oulainen!"/>
+                    <meta property="og:image" content="https://carfixoulainen.fi/static/media/headerImg.bc743134605778701916.jpg"/>
+                    <meta property="og:url" content="https://carfixoulainen.fi/"/>
+                    <title>CarFix Oulainen</title>
+                </Helmet>
+            </div>
+            <div className='header'>
+                <img src={logo} alt='CarFix Oulainen Logo' />
+            </div>
         </div>
     )
 }
